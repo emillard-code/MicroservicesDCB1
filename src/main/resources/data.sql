f38d8dc96e4bed858c08159b031ef73c7822b713
Emerson Millard <emillard.code@gmail.com>
2022-07-16 12:00:00 -0700

    Added data.sql file.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,4 @@ insert into department values (2, 'Department B','2nd Street', 'IT-002');
 insert into department values (3, 'Department C','3rd Street', 'IT-003');
 insert into department values (4, 'Department D','4th Street', 'IT-004');
 insert into department values (5, 'Department E','5th Street', 'IT-005');
+insert into department values (6, 'Department F','6th Street', 'IT-006');
