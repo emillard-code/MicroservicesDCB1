85c5a222a8b677dea65789c8b579bddbcb8dc11c
Emerson Millard <emillard.code@gmail.com>
2022-07-16 12:00:00 -0700

    Added method to controller class.

--- src/main/java/com/project/controller/DepartmentController.java
+++ src/main/java/com/project/controller/DepartmentController.java
@@ -4,10 +4,7 @@ import com.project.model.Department;
 import com.project.service.DepartmentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/departments")
@@ -22,6 +19,14 @@ public class DepartmentController {
 
         log.info("Inside saveDepartment method of DepartmentController");
         return  departmentService.saveDepartment(department);
+
+    }
+
+    @GetMapping("/{id}")
+    public Department findDepartmentById(@PathVariable("id") Long departmentId) {
+
+        log.info("Inside findDepartmentById method of DepartmentController");
+        return departmentService.findDepartmentById(departmentId);
 
     }
 
