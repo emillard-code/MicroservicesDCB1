d3d2c4f8ef0097f663f9c302b2b2bec69d89e62c
Emerson Millard <emillard.code@gmail.com>
2022-07-16 12:00:00 -0700

    Added controller class.

---
+++ src/main/java/com/project/controller/DepartmentController.java
@@ -0,0 +1,28 @@
+package com.project.controller;
+
+import com.project.model.Department;
+import com.project.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/departments")
+@Slf4j
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/")
+    public Department saveDepartment(@RequestBody Department department) {
+
+        log.info("Inside saveDepartment method of DepartmentController");
+        return  departmentService.saveDepartment(department);
+
+    }
+
+}
