e5d0b22edbf22b1c1e1f9cd5c1d16c677e20bbc6
Emerson Millard <emillard.code@gmail.com>
2022-07-17 12:00:00 -0700

    Modified data.sql file.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into department values (1, 'Department A','1st Street', 'IT-001');
-insert into department values (2, 'Department B','2nd Street', 'IT-002');
-insert into department values (3, 'Department C','3rd Street', 'IT-003');
-insert into department values (4, 'Department D','4th Street', 'IT-004');
-insert into department values (5, 'Department E','5th Street', 'IT-005');
-insert into department values (6, 'Department F','6th Street', 'IT-006');
+insert into department values (1, '1st Street','IT-001', 'Department A');
+insert into department values (2, '2nd Street','IT-002', 'Department B');
+insert into department values (3, '3rd Street','IT-003', 'Department C');
+insert into department values (4, '4th Street','IT-004', 'Department D');
+insert into department values (5, '5th Street','IT-005', 'Department E');
+insert into department values (6, '6th Street','IT-006', 'Department F');
