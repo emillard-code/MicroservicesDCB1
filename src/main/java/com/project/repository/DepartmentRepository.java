c4765e2ba0d3c0f5d3f27bd5a175c4893d773a6d
Emerson Millard <emillard.code@gmail.com>
2022-07-18 22:07:27 -0700

    Added repository class.

---
+++ src/main/java/com/project/repository/DepartmentRepository.java
@@ -0,0 +1,12 @@
+package com.project.repository;
+
+import com.project.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+
+}
