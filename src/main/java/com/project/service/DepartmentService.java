45ecea7f6c52868f7a7707a91dcc240b74a2d11d
Emerson Millard <emillard.code@gmail.com>
2022-07-18 22:19:03 -0700

    Added service class.

---
+++ src/main/java/com/project/service/DepartmentService.java
@@ -0,0 +1,23 @@
+package com.project.service;
+
+import com.project.model.Department;
+import com.project.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+
+    }
+
+}
