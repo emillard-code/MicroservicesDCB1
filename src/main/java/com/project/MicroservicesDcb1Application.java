c040b238e2378fa0ff7b2a455c5f6d559612fd97
Emerson Millard <emillard.code@gmail.com>
2022-07-18 21:31:11 -0700

    Minor format changes.

--- src/main/java/com/project/MicroservicesDcb1Application.java
+++ src/main/java/com/project/MicroservicesDcb1Application.java
@@ -1,4 +1,4 @@
-package com.project.microservicesdcb1;
+package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicroservicesDcb1Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MicroservicesDcb1Application.class, args);
+
     }
 
 }
